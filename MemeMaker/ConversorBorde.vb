7744c085f3e788f88d667661ad3063b955fd946c
Javier Catalá <javicatala@gmail.com>
2019-01-04 16:18:53 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/ConversorBorde.vb
@@ -0,0 +1,21 @@
+﻿Public Class ConversorBorde
+    Implements IValueConverter
+
+    Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object,
+                            ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+
+        If value Then
+            Return 1
+        Else
+            Return 0
+        End If
+
+    End Function
+
+
+    Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object,
+                                ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+
+End Class
---
+++ MemeMaker/MainWindow.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class MainWindow
+    Private Sub SeleccionarButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+        Dim result As Boolean = dlg.ShowDialog()
+
+        If result = True Then
+            MemeDockPanel.Background = New ImageBrush(New BitmapImage(New Uri(dlg.FileName)))
+        End If
+    End Sub
+End Class
